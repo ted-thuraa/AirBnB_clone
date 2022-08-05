c36d495a386ad3cbb00ee995a0bc77c716c86f58
austineoduor <austineoduor57@gmail.com>
2022-08-05 12:52:10 +0300

    tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,25 +1,86 @@
 #!/usr/bin/python3
+
 """
-Test suits for the base model
+    Defines a class TestBaseModel.
 """
 
 
-import uuid
-import unittest
-from time import sleep
-from datetime import datetime
 from models.base_model import BaseModel
+import unittest
+import models
+import os
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-"""my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))"""
-if __name__ == '__main__':
-    unittest.main()
+
+class TestBaseModel(unittest.TestCase):
+    """Represent a TestBaseModel."""
+
+    def setUp(self):
+        """SetUp method"""
+
+        self.base_model = BaseModel()
+        self.base_model.data = "Test data"
+
+    def TearDown(self):
+        """TearDown method."""
+
+        del self.base_model
+
+    def test_init_id(self):
+        """Test different id per object created"""
+
+        bm1 = BaseModel()
+        self.assertNotEqual(bm1.id, self.base_model.id)
+
+    def test_copy_object(self):
+        """Copy an object with the kwargs init from BaseModel"""
+
+        my_model_dict = self.base_model.to_dict()
+        bm1 = BaseModel(**my_model_dict)
+        self.assertEqual(bm1.id, self.base_model.id)
+
+    def test_id_type(self):
+        """Test the id type from BaseModel"""
+
+        self.assertIsInstance(self.base_model.id, str)
+
+    def test_docstring(self):
+        """Test docstring for the module and the class"""
+
+        self.assertIsNotNone(
+            models.base_model.__doc__,
+            "No docstring in the module"
+        )
+        self.assertIsNotNone(BaseModel.__doc__, "No docstring in the class")
+
+    def test_permissions_file(self):
+        """Test File base_model.py permissions"""
+
+        test_file = os.access("models/base_model.py", os.R_OK)
+        self.assertTrue(test_file, "Read permissions")
+        test_file = os.access("models/base_model.py", os.W_OK)
+        self.assertTrue(test_file, "Write Permissions")
+        test_file = os.access("models/base_model.py", os.X_OK)
+        self.assertTrue(test_file, "Execute permissions")
+
+    def test_mod_to_dict(self):
+        """Test dictionary representation in BaseModel"""
+
+        self.assertIsInstance(self.base_model.to_dict(), dict)
+
+    def test_type_object(self):
+        """Test type object of BaseModel"""
+
+        self.assertEqual(
+            str(type(self.base_model)),
+            "<class 'models.base_model.BaseModel'>")
+        self.assertIsInstance(self.base_model, BaseModel)
+
+    def test_str_representation(self):
+        """Test str representation of BaseModel"""
+
+        str_rep = "[{:s}] ({:s}) {:s}".format(
+            self.base_model.__class__.__name__,
+            self.base_model.id,
+            str(self.base_model.__dict__)
+        )
+        self.assertEqual(str_rep, str(self.base_model))
