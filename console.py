43a065115d18b9d6af8b9134f3f42f6585741700
ted-thuraa <mutwirited@gmail.com>
2022-08-09 19:32:03 +0300

    up

--- console.py
+++ console.py
@@ -1,186 +1,216 @@
 #!/usr/bin/python3
 """ console that contains the entry point """
 
-import cmd
-import models
-import re
+import cmd  # test
+from models.base_model import BaseModel
 import shlex
+from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import json
+
 
 class HBNBCommand(cmd.Cmd):
-    """console"""
-    prompt = "(hbnb) "
-    @classmethod
-    def fetch_command(cls, command):
-        """Gets the command typed in the console"""
-        commands = {"all": cls.do_all, "show": cls.do_show, "destroy": cls.do_destroy,
-        "update": cls.do_update, "count": cls.do_count, "create": cls.do_create}
-        if command in commands:
-            return commands[command]
-        else:
-            return None
+    """ The console for AirBnB,
+    written by Peter Wu and Bryan Leung """
 
-    def do_EOF(self, arg):
-        """Quit the program"""
-        return True
+    prompt = "(hbnb) "  # the intranet's required prompt
+    classes = ["BaseModel", "User", "State", "City",
+               "Amenity", "Place", "Review"]  # for tasks 8/ 9
 
-    def do_quit(self, arg):
-        """Quit the program"""
-        return True
+    def do_quit(self, line):  # getting rid of parameters throws errors
+        """Quit command to exit the program
+        """
+        exit()  # not sure if there is a desired return value
+        # This expects a return True but for now, exit works just fine.
+
+    def do_EOF(self, line):
+        """ EOF calls on quit """
+        print()  # for a new line
+        exit()  # assuming EOF function does the same as do_quit
 
     def emptyline(self):
         """ do nothing """
-        pass
+        pass  # will get a new line and aprompt
 
-    def do_create(self, arg):
+    # The above is for Console 0.0.1
+    # Below is for Console 0.1 or task 7 and onwards
+
+    def do_create(self, line):
         """ Creates a new instance of BaseModel, saves to a JSON file,
-        and prints the ID when finished  """
+        and prints the ID when finished """
 
-        if arg:
-            try:
-                new_instance = models.dummy_classes[arg]
-                new_instance = new_instance()
-                new_instance.save()
-                print(new_instance.id)
-            except:
-                print("** class doesn't exist **")
+        if len(line) < 1:  # if no arguments passed
+            print("** class name missing **")
+        elif line not in HBNBCommand.classes:
+            print("** class doesn't exist **")  # if class doesn't exist
         else:
-            print("** class name misssing **")
+            new = eval(line)()  # execute the string argument into a shell
+            print(new.id)  # prints the new id of the object we created
+            new.save()  # serialize into JSON file
 
-    def do_show(self, arg):
-        """Prints the string representation of an instance,
+    def do_show(self, line):  # shows the class and id
+        """ Prints the string representation of an instance,
         if given the class name and id """
 
-        if arg:
-            arg = arg.split()
-            if arg[0] in models.dummy_classes:
-                if len(arg) > 1:
-                    key = "{}.{}".format(arg[0], arg[1])
-                    try:
-                        print(models.storage.all()[key])
-                    except:
-                        print("** no instance found **")
-                else:
-                    print("** instance id missing **")
-            else:
-                print("** class doesn't exist **")
-        else:
+        classID = ""  # will store the input if >= 2 words
+        if len(line) < 1:  # user just typed show into CLI
             print("** class name missing **")
-
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id and saves changes"""
-        if arg:
-            arg = arg.split()
-            if arg[0] in models.dummy_classes:
-                if len(arg) > 1:
-                    key = "{}.{}".format(arg[0], arg[1])
-                    try:
-                        deleted = models.storage.all().pop(key)
-                        del(deleted)
-                        models.storage.save()
-                    except:
-                        print("** no instance found **")
-                else:
-                    print("** instance id missing **")
+            return  # not sure if we need a return value?
+        tokenize = shlex.split(line)  # tokenize the input with split
+        if tokenize[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")  # they typed a fake class
+        elif len(tokenize) < 2:  # user typed a class with no ID
+            print("** instance id missing **")
+        else:  # we have 2 or more arguments now. should unit test this
+            classID = tokenize[0] + "." + tokenize[1]
+            # combines the 2 strings- class with the id
+            if classID in storage.all().keys():
+                print(storage.all()[classID])  # prints the object!!
+            # matching class and id is found in our storage, success!!!
             else:
-                print("** class doesn't exist **")
-        else:
-            print("** class name missing **")
+                print("** no instance found **")  # bad
 
-    def do_all(self, arg):
+    def do_destroy(self, line):
+        """ Deletes an instance of an object if given
+        the class name and the ID. Then save the changes """
+
+        # pretty much exactly like do_show but add a del()
+        # lollll i just copy pasted the above
+        classID = ""
+        if len(line) < 1:
+            print("** class name missing **")
+            return  # not sure if we need a return value?
+        tokenize = shlex.split(line)
+        if tokenize[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")  # they typed a fake class
+        elif len(tokenize) < 2:  # user typed a class with no ID
+            print("** instance id missing **")
+        else:  # we have 2 or more arguments now. should unit test this
+            classID = tokenize[0] + "." + tokenize[1]
+            # combines the 2 strings- class with the id
+            if classID in storage.all().keys():
+                del storage.all()[classID]
+            else:
+                print("** no instance found **")
+            storage.save()
+
+    def do_all(self, line):
         """ Prints a string of all instances or if given the class name,
         of just the instances of the class name """
         # print all values in the storage.all() if no paremeters entered
         # otherwise print only values that match parameters entered
-        result = []
-        if arg:
-            arg = arg.split()
-            if arg[0] in models.dummy_classes:
-                for instance, obj in models.storage.all().items():
-                    if instance.split('.')[0] == arg[0]:
-                        result.append(str(obj))
-            else:
+        if (len(line) < 1):
+            print(["{}".format(v) for k, v in storage.all().items()])
+        # print all instances but does not print empty brackets if empty
+        else:  # first check if the class is in our list of classes
+            tokenize = shlex.split(line)
+            if tokenize[0] not in self.classes:
                 print("** class doesn't exist **")
-        else:
-            for instance, obj in models.storage.all().items():
-                result.append(str(obj))
-        if result:
-                print(result)
+            else:  # print everything we have
+                print(["{}".format(v) for k, v in storage.all().items()
+                       if type(v).__name__ in tokenize[0]])
 
-    def do_update(self, arg):
+    def do_update(self, line):
         """ Updates an instance based on the class name and ID.
-        Adds  """
-        if arg:
-            arg = arg.split()
-            if arg[0] in models.dummy_classes:
-                if len(arg) > 1:
-                    key = "{}.{}".format(arg[0], arg[1])
-                    try:
-                        instance = models.storage.all()[key]
-                        if len(arg) > 2:
-                            if len(arg) > 3:
-                                setattr(instance, arg[2], arg[3].strip('"'))
-                                instance.save()
-                            else:
-                                print("** value missing **")
-                        else:
-                            print("** attribute name missing **")
-                    except:
-                        print("** no instance found **")
-                else:
-                    print("** instance id missing **")
-            else:
-                print("** class doesn't exist **")
-        else:
+        Adds or updates attributes ans saves the changes """
+        # TOO MANY WORDS
+        # lots of same checks for line lenths and error messages,
+        # should expect a str len of 4.
+        # 0 is class, 1 is id, 2 is attr, 3 is value.
+        # try an if or try in case arguments are bad?!?!
+        if len(line) < 1:
             print("** class name missing **")
+        else:  # just number of arguments checking
+            tokenize = shlex.split(line)
+            if len(tokenize) == 3:
+                print("** value missing **")
+            elif len(tokenize) == 2:
+                print("** attribute name missing **")
+            elif len(tokenize) == 1:
+                print("** instance id missing **")
+            else:  # of arguments is correct, now check if class exists
+                if tokenize[0] not in self.classes:
+                    print("** class doesn't exist **")
+                else:  # check if the id exists
+                    key = tokenize[0] + "." + tokenize[1]
+                    if key not in storage.all():
+                        print("** no instance found **")
+                    else:  # we have a match! now check the data types
+                        obj = storage.all().get(key, 0)
+                        try:  # we will try to set attr
+                            setattr(obj, tokenize[2], type(getattr(obj,
+                                    tokenize[2]))(tokenize[3]))
+                        except AttributeError:  # the attr data type is bad
+                            try:  # try with int casting, it it works yey
+                                val = int(tokenize[3])
+                            except ValueError:
+                                try:  # try with float cause int didnt work
+                                    val = float(tokenize[3])
+                                except ValueError:
+                                    val = str(tokenize[3])  # try with str
+                            setattr(obj, tokenize[2], val)  # set attribute
+                        storage.save()  # save the object to storage
 
-    def do_count(self, arg):
+    def do_count(self, line):
         """ counts number of objects of specified class """
-        count = 0
-        if args:
-            arg = arg.split()
-            if arg[0] in models.dummy_classes:
-                for instance, obj in models.storage.all().items():
-                    count += 1
-            else:
+        if len(line) < 1:  # checks id there is a class name
+            print("** class name missing **")
+        else:  # We split the input and if class given exists
+            tokenize = shlex.split(line)
+            if tokenize[0] not in self.classes:
                 print("** class doesn't exist **")
-        else:
-            for instance, obj in models,storage.all().items():
-                count += 1
-        print(count)
+            else:  # we have a counter now for each class
+                cnt = 0
+                obj = storage.all()  # obj is our data storage
+                for k, v in obj.items():  # this increases our counter
+                    if type(v).__name__ == tokenize[0]:
+                        cnt += 1  # by matching all class names
+                print(cnt)
 
     def default(self, line):
-        """
-        handle invalid commands and
-        special commands 
-        """
-        match = re.fullmatch(r"[A-Za-Z]+\.[A-Za-z]+\(.*?\)", line)
-        if match:
-            splited = line.split('.')
-            if splited[0] in models.dummy_classes:
-                parsed = splited[1].split("(")
-                parsed[1] = parsed[1].strip(")")
-                args = parsed[1].split(",")
-                args = [arg.strip() for arg in args]
-                if len(args) >= 3:
-                    temp = args[2]
-                    args = [arg.strip('"') for arg in args[:2]]
-                    args.append(temp)
-                else:
-                    args = [arg.strip('"') for arg in args]
-                command = self.fetch_command(parsed[0])
-                if command:
-                    reconstructed_args = [arg for arg in args]
-                    reconstructed_args.insert(0, splited[0])
-                    reconstructed_command = " ".join(reconstructed_args)
-                    command(self, reconstructed_command)
-                else:
-                    print("*** Unknown syntax: {}".format(line))
+        """default when user type in class using <class name>.all()"""
+        methods = {"all": self.do_all, "count": self.do_count,
+                   "show": self.do_show, "destroy": self.do_destroy,
+                   "update": self.do_update}
+        # we have a dict for the advance problems. we already coded the
+        # basic functionalities the advance problems require. so we will just
+        # redirect and call on those functions that we have made.
+        # Peter did this part. he did a lot of string manipulation with . as
+        # a delimiter but i would have tried to add . as a white space
+        # attribute in the shlex arguments.
+        key = line.split(".")
+        if len(key) < 2:
+            print("** missing arguments **")
+        else:  # lol peter did some crazy string manipulation
+            subkey = key[1].split("(")
+            if subkey[0] not in methods:
+                print("** invalid command **")
             else:
-                print("** class doesn't exist **")
-        else:
-            print("*** Unknown syntax: {}".format(line))
+                subkey[1] = subkey[1].replace(")", "")
+                if "{" in subkey[1]:
+                    subkey[1] = subkey[1].replace(',', ':', 1)
+                    t = '{' + subkey[1] + '}'
+                    t = t.replace("'", '"')
+                    d = {}
+                    try:
+                        d = json.loads(t)
+                    except:
+                        print("** invalid format **")
+                        return
+                    for k, v in d.items():
+                        for k1, v1 in v.items():
+                            ustr = ""
+                            ustr = key[0] + " " + k + " " + k1 + " " + str(v1)
+                            methods[subkey[0]](ustr)
+                else:
+                    subkey[1] = subkey[1].replace(",", " ")
+                    methods[subkey[0]](key[0] + " " + subkey[1])
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # protects against execution when imported
     HBNBCommand().cmdloop()  # recursively loops back until exited or errors
